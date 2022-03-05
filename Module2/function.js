951d33d5236d0137bd057d971a28a021d6dbb719
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-03-05 23:14:40 +0530

    function

---
+++ Module2/function.js
@@ -0,0 +1,41 @@
+//declaration of function without parameter.
+function sayhello(){
+    console.log("Hello to function ");
+}
+sayhello();
+
+//declaration of function with parameter/
+
+function sum(n1,n2){
+    let addition = n1 + n2;
+    console.log("Addition of given number is "+addition);
+}
+sum(2,3);
+
+//Function with return type.
+function multiple(n1,n2){
+    return(n1*n2);
+}
+let ans = multiple(3,5);
+console.log(ans);
+
+//function are first class citizen in js
+let a = function sub(n1,n2){
+    return(n1 - n2);
+}
+console.log(a(5,3));
+
+//IIFE -> Immediate invoke function expression
+//IIFE without para
+(function(){
+    console.log("Hello IIFE");
+})();
+
+//IIFE with para
+(function(n1,n2){
+    console.log(n1/n2);
+})(10,5);
+
+
+
+
--- Module2/intro.js
+++ Module2/intro.js
@@ -1,1 +1,38 @@
+//printing in js
 console.log("Hello World");
+
+//variable in js and data types
+let num = 10; //Number
+console.log(num);
+
+let char = 'a'; //Character
+console.log(char);
+
+let string = 'I am a string.'; //String
+console.log(string);
+
+let bool = true; //boolean
+console.log(bool);
+
+//Loops
+for(let i = 1 ; i <= 5 ; i++){
+    console.log(i);
+}
+
+//Number is prime or not
+let n = 13;
+let count = 0;
+
+for(let i = 2 ; i <= (n-1) ; i++){
+    if(n % i == 0){
+        count++;
+        break;
+    }
+
+}
+
+if(count == 0){
+    console.log("prime");
+}else{
+    console.log("not prime");
+}
