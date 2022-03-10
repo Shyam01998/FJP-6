228c8d2c8e9d93b425eae44d919d13eeeb6628f3
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-03-11 00:17:26 +0530

    Dev

---
+++ Module2/node js/module/testmodule.js
@@ -0,0 +1,11 @@
+
+let calc = require("./calculator.js");
+//console.log(calc);
+
+console.log(calc.addition(4,6));
+
+console.log(calc.subtraction(5,3));
+
+console.log(calc.multiplication(3,2));
+
+console.log(calc.division(10,5));
