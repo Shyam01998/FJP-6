f70a57700853a69ac23c3840c712f07e3c650b72
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-03-12 23:10:13 +0530

    create dir

--- Module2/node js/fs.js
+++ Module2/node js/fs.js
@@ -3,6 +3,7 @@ let fs = require("fs");
 
 let path = require("path");
 
+/* create a file 
 let filePath = path.join(__dirname,"file.txt");
 console.log(filePath);
 
@@ -22,3 +23,29 @@ console.log(fs.readFileSync(filePath,"utf-8"));
 
 //D-delete
 fs.unlinkSync(filePath);
+
+*/
+
+/* create a directory */
+if (!fs.existsSync("hamariDirectory")) {
+    fs.mkdirSync("hamariDirectory");
+}
+
+//read a directory
+let folderPath = __dirname;
+let contentofFolder = fs.readdirSync(folderPath);
+console.log(contentofFolder);
+
+//delete a directory
+fs.rmdirSync("hamariDirectory");
+
+//copy a file 
+let source = path.join(__dirname,"file.txt");
+let destination = path.join(__dirname, "module" , "file.txt");
+console.log(source);
+console.log(destination);
+
+ fs.copyFileSync(source,destination);
+
+
+
