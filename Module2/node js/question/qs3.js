b7defac89b4b7382a923fbf8efffafb93dab9b69
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-03-18 00:41:44 +0530

    node question

--- Module2/node js/question/qs3.js
+++ Module2/node js/question/qs3.js
@@ -1,1 +1,11 @@
 //copy index.html file from module1 to a new folder inside module2 having name html.
+let fs = require("fs");
+let path = require("path");
+
+let srcPath = path.join(__dirname ,".." , ".." ,".." ,"Module1" , "index.html");
+console.log("source -->",srcPath);
+
+let destPath = path.join(__dirname , "..","..","index.html");
+console.log("destination-->" ,destPath);
+
+fs.copyFileSync(srcPath , destPath)
--- Module2/node js/question/qs5.js
+++ Module2/node js/question/qs5.js
@@ -1,1 +1,13 @@
 //move a file
+
+let fs = require("fs");
+let path = require("path");
+ 
+let srcPath = path.join(__dirname , "abcd.txt");
+// console.log(srcPath);
+
+let destPath = path.join(__dirname, "newFolder","abcd.txt");
+// console.log(destPath);
+fs.copyFileSync(srcPath , destPath);
+
+fs.unlinkSync(srcPath);
