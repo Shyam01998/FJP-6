797bfe2a4d16472c14f89a086c90131b4dbaf31d
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-06-16 18:23:33 +0530

    this.js

--- Module4/Reactprerequiste/spread.js
+++ Module4/Reactprerequiste/spread.js
@@ -1,12 +1,12 @@
 //spread operator with array
 
-// let arr = [1,2,3,5,6,7,8];
-// let arr2 = [...arr]
+//  let arr = [1,2,3,5,6,7,8];
+//  let arr2 = [...arr]
 
-// arr2[2] = 10;
+//  arr2[2] = 10;
 
-// console.log(arr);
-// console.log(arr2);
+ //console.log(arr);
+//console.log(arr2);
 
 //spread operator with objects;
 
@@ -20,8 +20,8 @@ let obj = {
         }
     }
 }
-//shallow copy
-let obj2 = { ...obj };
+ //shallow copy
+ let obj2 = { ...obj };
 
 obj2.name = "Aman";
 
@@ -29,16 +29,16 @@ obj2.name = "Aman";
 obj2.address.country = "India";
 
 
-// console.log(obj.name);
-// console.log(obj.address);
-// console.log("####################")
-// console.log(obj2.name);
-// console.log(obj2.address);
+ console.log(obj.name);
+ console.log(obj.address);
+ console.log("####################")
+ console.log(obj2.name);
+ console.log(obj2.address);
 
 
-//deep copy
-let obj3 = JSON.parse(JSON.stringify(obj))
+// //deep copy
+ let obj3 = JSON.parse(JSON.stringify(obj))
 
-obj3.address.country = "Japan";
-console.log(obj.address.country);
-console.log(obj3.address.country);
+ obj3.address.country = "Japan";
+ console.log(obj.address.country);
+ console.log(obj3.address.country);
