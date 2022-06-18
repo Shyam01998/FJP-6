b800ba78dd026f8f946abef0251b1008e29e325c
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-06-18 12:37:13 +0530

    react npx

--- Module4/React/todo-app/src/component/Todo.js
+++ Module4/React/todo-app/src/component/Todo.js
@@ -3,9 +3,7 @@ import { Component } from "react";
 class Todo extends Component{
     render(){
         return(
-                <div>
-                    This is my Component.
-                </div>
+                <div>This is Todo Component.</div>
              )
     }
 }
