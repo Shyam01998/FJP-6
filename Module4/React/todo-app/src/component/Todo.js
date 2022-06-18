fd7c52699a6406526bfa8a5cf1edf68734d58d7e
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-06-18 12:26:40 +0530

    react npx

---
+++ Module4/React/todo-app/src/component/Todo.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Todo extends Component{
+    render(){
+        return(
+                <div>
+                    This is my Component.
+                </div>
+             )
+    }
+}
+export default Todo;
+
