a0c0f76eb0d8e1f545950b28ed3e5976d01dbfe0
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-06-18 23:47:33 +0530

    todo-app complete

--- Module4/React/todo-app/src/component/Todo.js
+++ Module4/React/todo-app/src/component/Todo.js
@@ -1,28 +1,54 @@
 import { Component } from "react";
 
-class Todo extends Component{
-    constructor(){
+
+class Todo extends Component {
+    constructor() {
         super();
         this.state = {
-            task:["Learn JS","Learn DOM","Learn React","Learn Node"]
+            tasks: [],
+            currTask: ""
         }
     }
-    render(){
-        return(
-                <div>
-                    <input type="text"/>
-                    <button>Add Task</button>
-                    <ul>
-                        {this.state.task.map((ele)=>(
-                            <li>
-                                <p>{ele}</p>
-                            </li>
-                        ))}
-                    </ul>
 
-                </div>
-             )
+    handleChange = (e) => {
+        // console.log(e.target.value);
+        this.setState({
+            currTask: e.target.value
+        })
+    }
+
+    handleAddTask = () => {
+        this.setState({
+            tasks: [...this.state.tasks, { task: this.state.currTask, id: this.state.tasks.length + 1 }],
+            currTask: ""
+        })
+    }
+
+    handleDelete = (id) => {
+        let narr = this.state.tasks.filter((taskObj) => {
+            return taskObj.id != id
+        })
+        this.setState({
+            tasks: [...narr]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.currTask} onChange={this.handleChange} />
+                <button onClick={this.handleAddTask}>Add Task</button>
+                <ul>
+                    {this.state.tasks.map((taskObj) => (
+                        <li key={taskObj.id}>
+                            <p>{taskObj.task}</p>
+                            <button onClick={() => this.handleDelete(taskObj.id)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
     }
 }
-export default Todo;
 
+export default Todo;
