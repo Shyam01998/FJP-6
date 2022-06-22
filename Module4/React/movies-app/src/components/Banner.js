77573f655759082b2efe02c7bfb4b937be1b855b
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-06-22 17:23:09 +0530

    Navbar and banner styling

--- Module4/React/movies-app/src/components/Banner.js
+++ Module4/React/movies-app/src/components/Banner.js
@@ -4,15 +4,17 @@ import { Component } from "react";
 class Banner extends Component{
     render(){
         return(
-            <div className="card">
-                <img src="https://www.trustedreviews.com/wp-content/uploads/sites/54/2020/09/marvel-spider-man-miles-morales-iz-1920x1080-1.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                    
+            <div className="card banner-card">
+                <img src="https://www.trustedreviews.com/wp-content/uploads/sites/54/2020/09/marvel-spider-man-miles-morales-iz-1920x1080-1.jpg" className="card-img-top banner-img" alt="..."/>
+                    <h1 className="card-title banner-title">Spider-Man Miles Moral</h1>
+                    <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                   
                         
                         
-                    </div>
-            </div>
+             </div>
+    
         )
     }
 }
 export default Banner;
+  
--- Module4/React/movies-app/src/components/NavBar.js
+++ Module4/React/movies-app/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 class NavBar extends Component{
     render(){
         return(
-            <div style ={{display : "flex" , padding: '0.5rem'}}>
+            <div style ={{display : "flex" , padding: '0.5rem',color:"blue"}}>
                 <h1>Movie App</h1>
                 <h2 style={{ marginLeft: '2rem', marginTop: '0.5rem' }}> Favourite</h2>
                 
