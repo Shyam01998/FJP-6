6499ccdc1789fa139717611849b29698795b839d
shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-12 21:58:35 +0530

    Ue2 and 3

---
+++ Module4/React/functional-comp-react/src/components/Ue2.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+
+function Ue2(){
+    const [count,setCount]= useState(0);
+    const[name,setName]=useState("Shyam");
+
+    useEffect(()=>{
+        document.title = `This is my Title ${count} ${name}`
+    },[])
+
+    return(
+        <div>
+            <h1> This is a Counter {count}   </h1>
+            <h1>{name}</h1>
+            <button onClick={()=>setCount(count + 1)}>+ 1</button>
+            <button onClick={()=>setName("Saurabh")}>ChangeName</button>
+        </div>
+    )
+}
+
+export default Ue2
