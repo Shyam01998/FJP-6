7ed5d028e7c2d18999f2169ec551529a0628d8bd
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-15 23:56:05 +0530

    assignment

---
+++ assignment/js program/arr.js
@@ -0,0 +1,56 @@
+/* Maximum value in an array */
+
+// let arr = [1,35,7,12,15];
+// let max;
+
+// max = arr[0];
+
+// for(let i = 1; i < arr.length;i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//     }
+// }
+
+// console.log("Maximum value in an array ="+max);
+
+
+/* Minimum value in an array */
+
+// let arr = [1, 35, 7, 12, 15];
+// let min;
+
+// min = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         min = arr[i];
+//     }
+// }
+
+// console.log("Minimum value in an array =" + min);
+
+/* Find element in an array */
+
+// let arr = [10, 15, 17, 13, 14, 25,19]
+// let ele = 15;
+
+// let a = findElement(arr , ele);
+// console.log(a);
+
+// function findElement(arr , ele){
+
+// for(let i = 0 ; i < arr.length; i++){
+//     if(arr[i] == ele){
+//         //console.log("Element present at " + i + " position");
+//         return i;
+        
+//     }
+// }
+//  //console.log("Element not found");
+
+//  return -1;
+// }
+
+
+
+
