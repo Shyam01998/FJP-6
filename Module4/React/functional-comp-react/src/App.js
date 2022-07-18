5f6859f5f3f0611d5ba01651a09fa7a414d96656
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-18 22:40:37 +0530

    context memo

--- Module4/React/functional-comp-react/src/App.js
+++ Module4/React/functional-comp-react/src/App.js
@@ -4,12 +4,24 @@ import Counter from './components/Counter';
 import Ue1 from './components/Ue1';
 import Ue2 from './components/Ue2';
 import Ue3 from './components/Ue3';
+import Parent from './components/Context/Parent';
+import Test from './components/Context/Test';
+import ThemeChanger from './components/Context/theme/ThemeChanger';
+import ContextNormal from './components/ContextNormalvsMemo/ContextNormal';
+import ContextMemo from './components/ContextNormalvsMemo/ContextMemo';
+
+
 
 function App() {
   return (
     //<Counter/>
-    
-    <Ue3/>
+    //<Ue3/>
+    //<Parent/>
+    //<Test/>
+    //<ThemeChanger/>
+    // <ContextNormal/>
+    <ContextMemo/>
+
   );
 }
 
