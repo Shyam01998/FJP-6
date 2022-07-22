76edba3e971e338bf592cdb41736516ff31a14e6
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-22 23:57:31 +0530

    login sample

--- Module4/React/reels-app/src/components/Login.jsx
+++ Module4/React/reels-app/src/components/Login.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 
 
 function Login(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const trackEmail = function (e){
+        setEmail(e.target.value);
+    }
+
+    const trackPassword = function(e){
+        setPassword(e.target.value);
+    }
+
+    const printDetails = function(){
+       // console.log(email+""+password);
+        alert(email + "" + password);
+    }
+
+
     return(
         <>
-        Login
+        <input type="text" onChange={trackEmail} placeholder="email"/>
+        <br></br>
+        <input type="text" onChange={trackPassword} placeholder="password"/>
+        <br></br>
+        <button value="click" onClick={printDetails}>Login</button>
         </>
     )
 }
---
+++ Module4/React/reels-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAN4ZWcr--aIkxaXQlo5ENn22U0ZZkY2CI",
+    authDomain: "class-demo-6a010.firebaseapp.com",
+    projectId: "class-demo-6a010",
+    storageBucket: "class-demo-6a010.appspot.com",
+    messagingSenderId: "208090665486",
+    appId: "1:208090665486:web:fc0b255e165a51998ae25f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
