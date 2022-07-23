79520e3a6d3706229652b069237aa37ba2cce96a
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-23 23:21:52 +0530

    firebase updated

--- Module4/React/firebase-demo/src/firebase.js
+++ Module4/React/firebase-demo/src/firebase.js
@@ -15,3 +15,29 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import  {getAuth} from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDWk--fmdcMTa7DXW9ZgiqwsesMpqbTK7s",
+//   authDomain: "reels-project-4426a.firebaseapp.com",
+//   projectId: "reels-project-4426a",
+//   storageBucket: "reels-project-4426a.appspot.com",
+//   messagingSenderId: "610510418412",
+//   appId: "1:610510418412:web:1b47c01d2c3d403f7210c9"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
