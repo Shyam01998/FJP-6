dbc9ca5287958990c6881d79abb65bb9c858078c
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-23 13:22:35 +0530

    sigin functionality added

--- Module4/React/reels-app/src/components/Signup.jsx
+++ Module4/React/reels-app/src/components/Signup.jsx
@@ -1,8 +1,25 @@
-
+import { useState } from "react";
+import {auth} from '../firebase';
+import {createUserWithEmailAndPassword} from 'firebase/auth'
 
 function Signup(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [name,setName] = useState("");
+
+    async function processSignup(){
+        let userCred = await createUserWithEmailAndPassword(auth,email,password)
+        console.log(userCred.user);
+
+    }
+
     return (
-        <div>Signup</div>
+        <>
+        <input type="email"  onChange={(e)=>{setEmail(e.target.value)}}  value={email}  placeholder="email"></input><br></br>
+        <input type="password" onChange={(e)=>{setPassword(e.target.value)}}  value={password}  placeholder="password"></input><br></br>
+        <input type="text"   onChange={(e)=>{setName(e.target.value)}}      value={name}   placeholder="Full Name"></input><br></br> 
+        <button value="click"  onClick={processSignup}>Sign up</button>
+        </>
     )
 }
 
