0fde3182f4a14f63d069689893f1fc58f8f066c8
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-07-28 22:52:15 +0530

    update

--- Module4/React/reels-app/src/components/Feed.jsx
+++ Module4/React/reels-app/src/components/Feed.jsx
@@ -15,34 +15,55 @@ function Feed(){
             <button onClick={()=>{auth.signOut()}}>Logout</button>
         </div>
         <div className="main_container">
-        <input type="file" 
-                onClick={(e)=>{
-                    console.log(e)
-                }} 
+        <input type="file"
+                    onClick={(e) => {
+                        console.log(e)
+                    }}
 
-                onChange={(e)=>{
-                    let videoObj = e.currentTarget.files[0];
-                    console.log(videoObj)
-                    let {name,size,type} = videoObj
-                    type = type.split("/")[0];
-                    if(type!=="video"){
-                        alert("Please upload a video");
-                    }else{
-                        let storageRef = ref(storage,`posts/${name}`)
-                        const uploadTask = uploadBytesResumable(storageRef, videoObj);
-                        uploadTask.on("state_changed",null,null,()=>{
-                            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                                console.log('File available at', downloadURL);
-                              });  
-                        })
-                    }
-                }}
-
+                    onChange={(e) => {
+                        let videoObj = e.currentTarget.files[0];
+                        console.log(videoObj)
+                        let { name, size, type } = videoObj
+                        console.log(size);
+                        type = type.split("/")[0];
+                        if (type !== "video") {
+                            alert("Please upload a video");
+                        } else {
+                            let storageRef = ref(storage, `${name}`)
+                            const uploadTask = uploadBytesResumable(storageRef, videoObj);
+                            uploadTask.on('state_changed',
+                                (snapshot) => {
+                                    // Observe state change events such as progress, pause, and resume
+                                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                                    console.log('Upload is ' + progress + '% done');
+                                    switch (snapshot.state) {
+                                        case 'paused':
+                                            console.log('Upload is paused');
+                                            break;
+                                        case 'running':
+                                            console.log('Upload is running');
+                                            break;
+                                    }
+                                },
+                                (error) => {
+                                    // Handle unsuccessful uploads
+                                },
+                                () => {
+                                    // Handle successful uploads on complete
+                                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                                        console.log('File available at', downloadURL);
+                                    });
+                                }
+                            );
+                        }
+                    }}
                 ></input>
 
 
 
-            <div className="upload_container">Upload</div>
+            
             <div className="reels_container"><VideoCard/></div>
         </div>
         </>
--- Module4/React/reels-app/src/components/VideoCard.jsx
+++ Module4/React/reels-app/src/components/VideoCard.jsx
@@ -3,7 +3,7 @@ import './videoCard.css';
 
 
 const  VideoCard = ()=>{
-    let [playing,setPlaying] = useState(false);
+    let [playing,setPlaying] = useState(true);
     let [commentBoxOpen,setCommentBox]= useState(false);
     return(
        <div className="video-card">
@@ -38,7 +38,7 @@ const  VideoCard = ()=>{
                 </div>
             ) : ("")}
 
-        <video onClick={(e)=>{
+        <video className="video-card-video" onClick={(e)=>{
             if(playing){
                 e.currentTarget.pause();
                 setPlaying(false)
@@ -46,7 +46,7 @@ const  VideoCard = ()=>{
                 e.currentTarget.play();
                 setPlaying(true);
             }
-        }}src=""></video>
+        }}src="https://firebasestorage.googleapis.com/v0/b/class-demo-28b22.appspot.com/o/production%20ID_4678261.mp4?alt=media&token=1e88c2b2-1603-44dc-b521-56241c88f990" crossrigin="anonymous"></video>
        </div>
     )
 }
