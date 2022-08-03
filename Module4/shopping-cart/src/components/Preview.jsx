c7f3958e50f1b0e4778b9df55e28185ac29784c7
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-08-03 22:49:43 +0530

    preview added

--- Module4/shopping-cart/src/components/Preview.jsx
+++ Module4/shopping-cart/src/components/Preview.jsx
@@ -1,9 +1,18 @@
-
+import "./Preview.css"
 
 let Preview=()=>{
     return(
         <>
-        Preview
+        <div className="preview-container">
+            <div className="preview-img-container">
+                 <img src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/x/j/s/iphone-11-128-d-mwm02hn-a-apple-0-original-imafkg242ugz8hwc.jpeg?q=70" alt="Image not available"/>
+             </div>
+            <div className="preview-listing">
+                <h2>IPhone</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid nisi tenetur sapiente explicabo alias possimus deserunt quae molestiae beatae dicta optio suscipit quam repellat nam soluta dignissimos ipsam, consectetur at doloremque earum eos ducimus eveniet. Nesciunt similique ipsum aspernatur natus.</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
         </>
     )
 }
