3e501f279cd9f443ab6a83394083140cbac715c0
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-08-04 23:06:27 +0530

    add to cart functionality added in preview add to cart

--- Module4/shopping-cart/src/components/Preview.jsx
+++ Module4/shopping-cart/src/components/Preview.jsx
@@ -1,12 +1,15 @@
 import "./Preview.css";
 import {useParams} from 'react-router';
 import {useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux'
+import { addCreator } from "../redux/actions";
 
 
-let Preview=()=>{
+let Preview=(props)=>{
     let {id} = useParams();
     let state = useSelector((state)=>state);
     let reqObject =state[id];
+    let dispatch = useDispatch();
     return(
         <>
         <div className="preview-container">
@@ -16,7 +19,11 @@ let Preview=()=>{
             <div className="preview-listing">
                 <h2>{reqObject.name}</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid nisi tenetur sapiente explicabo alias possimus deserunt quae molestiae beatae dicta optio suscipit quam repellat nam soluta dignissimos ipsam, consectetur at doloremque earum eos ducimus eveniet. Nesciunt similique ipsum aspernatur natus.</p>
-                <button>Add to Cart</button>
+                <button
+                onClick={()=>{
+                    dispatch(addCreator(id))
+                    alert("Added to cart!!")
+                }}>Add to Cart</button>
             </div>
         </div>
         </>
