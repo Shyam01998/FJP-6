424274a536be7c289ffea5602e721878ded69180
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-08-06 23:13:37 +0530

    header and template

--- Module4/resume-builder/src/components/header.jsx
+++ Module4/resume-builder/src/components/header.jsx
@@ -1,9 +1,22 @@
-
+import "./header.css"
+import {Link} from 'react-router-dom'
 
 const Header= ()=>{
     return (
         <>
-        <h1>Header</h1>
+        <div className="header">
+            <div className="header-logo">
+                <Link to="/"> <img src="Module4\resume-builder\src\static\images\logo.png" alt="Image not available" /> </Link>
+            </div>
+            <div className="header-links">
+                <ul>
+                    <li><Link to="/template">Resume Templates</Link></li>
+                    <li><Link to="/about">About us</Link></li>
+                    <li><Link to="/register"><button className="btn">Register</button></Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </div>
+        </div>
         </>
     )
 }
--- Module4/resume-builder/src/components/template.jsx
+++ Module4/resume-builder/src/components/template.jsx
@@ -1,10 +1,57 @@
-
+import "./template.css"
+import skin1 from "../static/images/skin1.svg";
+import skin2 from "../static/images/skin2.svg";
+import skin3 from "../static/images/skin3.svg";
+import skin4 from "../static/images/skin4.svg";
+import skin5 from "../static/images/skin5.svg";
+import skin6 from "../static/images/skin6.svg";
+import skin7 from "../static/images/skin7.svg";
+import skin8 from "../static/images/skin8.svg";
 
 
 const Template= ()=>{
     return (
         <>
-        <h1>Templete</h1>
+        <div className="templates">
+            <div className="templates-intro">
+                <h1>Select a resume template</h1>
+                <p>You can edit and change it later!</p>
+            </div>
+            <div className="templates-style">
+                <div className="template">
+                    <img src={skin1} alt="template not available" ></img>
+                    <button className="template-btn">USE TEMPLATE</button>
+                </div>
+            
+                <div className="template">
+                    <img src={skin2} alt="template not available" ></img>
+                </div>
+            
+                {/* <div className="template">
+                    <img src={skin3} alt="template not available" ></img>
+                </div>
+            
+                <div className="template">
+                    <img src={skin4} alt="template not available" ></img>
+                </div>
+            
+                <div className="template">
+                    <img src={skin5} alt="template not available" ></img>
+                </div>
+            
+                <div className="template">
+                    <img src={skin6} alt="template not available" ></img>
+                </div>
+          
+                <div className="template">
+                    <img src={skin7} alt="template not available" ></img>
+                </div>
+            
+                <div className="template">
+                    <img src={skin8} alt="template not available" ></img>
+                </div> */}
+            </div>
+        </div>
         </>
     )
 }
