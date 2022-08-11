fdf060e2cc15b97c41802c5ff1c5d6bb0d446d68
Shyam01998 <shyamsundarsahoo98@gmail.com>
2022-08-11 22:55:58 +0530

    add upto nodemon

---
+++ Module5/readme.txt
@@ -0,0 +1,7 @@
+npm init -y
+
+npm install express
+
+npm install -g nodemon
+
+add "start":"nodemon server.js" to package.json
--- Module5/server.js
+++ Module5/server.js
@@ -5,15 +5,22 @@ const express = require("express");
 
 const app = express();
 
-//get karna he sayHello
+//get karna he data from  sayHello
 app.get("/sayHello",function(req , res){
     res.send("Hello from Server");
 })
 
-//get karna he sayBye
+//get karna he data from say
+app.get("/say",function(req,res){
+    res.send("HomePage")
+})
+
+//get karna he data fromm    sayBye
 app.get("/sayBye",function(req,res){
     res.send("Bye from server");
 })
+
+
 
 app.listen(3000,function(){
     console.log("Server started at port 3000")
